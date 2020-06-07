b75bc3188a2fcd3d3478b4dc84a9cf8abceef432
Diego Clair <diego93rodrigues@gmail.com>
2020-06-07 14:08:21 -0300

    Finished initial challenge structure

---
+++ devops/docker/challenge03-CICD-Go/go.mod
@@ -0,0 +1,3 @@
+module github.com/diegoclair/microservices-codeeducation/tree/master/devops/docker/challenge03-CICD-Go
+
+go 1.14
--- devops/docker/challenge03-CICD-Go/main.go
+++ devops/docker/challenge03-CICD-Go/main.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/diegoclair/microservices-codeeducation/tree/master/devops/docker/challenge03-CICD-Go/function"
+)
+
 func main() {
+	result := function.AddTwoNumbers(5, 5)
+	fmt.Println(result)
 
 }
