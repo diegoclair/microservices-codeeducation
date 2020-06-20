e4436bc0a6c96ea725b326adead82e9a9bb7d717
Diego Clair <diego93rodrigues@gmail.com>
2020-06-20 19:23:14 -0300

    Change package name

--- devops/docker/challenge04-ks8/challenge03-golang/main.go
+++ devops/docker/challenge04-ks8/challenge03-golang/main.go
@@ -4,11 +4,13 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
+	_ "github.com/diegoclair/microservices-codeeducation/devops/docker/challenge04-ks8/challenge03-golang/greeting"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
 
-	fmt.Fprintf(w, greeting("Code.education Rocks!"))
+	fmt.Fprintf(w, greeting.greeting("Code.education Rocks!"))
 }
 
 func main() {
