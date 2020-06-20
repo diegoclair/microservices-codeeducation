0670c5ab054a5035c1487cbf3d01c9f888295010
Diego Clair <diego93rodrigues@gmail.com>
2020-06-20 18:52:50 -0300

    Updated error message

--- devops/docker/challenge03-CI-Go/src/soma/main_test.go
+++ devops/docker/challenge03-CI-Go/src/soma/main_test.go
@@ -5,6 +5,6 @@ import "testing"
 func TestSoma(t *testing.T) {
 	result := soma(5, 5)
 	if result != 10 {
-		t.Errorf("soma did not work, expected 10! but got %v", result)
+		t.Errorf("soma function did not work, expected 10! but got %v", result)
 	}
 }
