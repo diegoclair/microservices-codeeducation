70247f59b1a068469e248319c0016d9dae15fd9b
Diego Clair <diego93rodrigues@gmail.com>
2020-08-06 00:53:26 -0300

    Update mysql docker image to 8 to use function uuid() as default value on uuid field

--- microservice-videos/data/factory/factory.go
+++ microservice-videos/data/factory/factory.go
@@ -4,30 +4,29 @@ import (
 	"strconv"
 
 	"github.com/GuiaBolso/darwin"
-	uuid "github.com/satori/go.uuid"
 )
 
 //FakeData to include fake data on database
 func FakeData(migrationsList []darwin.Migration, howManyLines int) []darwin.Migration {
 	init := len(migrationsList)
 	size := len(migrationsList) + howManyLines
-	var values, UUID string
+	var values string
 	for i := init; i < size; i++ {
-		UUID = uuid.NewV4().String()
 		if (i + 1) == size {
 
-			values += `('` + UUID + `', 'test ` + strconv.Itoa(i) + `','description ` + strconv.Itoa(i) + `');`
+			values += `('test ` + strconv.Itoa(i) + `','description ` + strconv.Itoa(i) + `');`
 			break
 		}
-		values += `('` + UUID + `', 'test ` + strconv.Itoa(i) + `','description ` + strconv.Itoa(i) + `'), `
+		values += `('test ` + strconv.Itoa(i) + `','description ` + strconv.Itoa(i) + `'), `
 	}
+
 	var (
 		migration = darwin.Migration{
 			Version:     float64(init + 1),
 			Description: "Inserting data tab_categories",
 			Script: `
 					INSERT INTO tab_categories 
-						(uuid,name,description) 
+						(name,description) 
 					VALUES ` + values,
 		}
 	)
--- microservice-videos/data/migrations/migrations.go
+++ microservice-videos/data/migrations/migrations.go
@@ -10,7 +10,7 @@ var (
 			Description: "Creating table tab_categories",
 			Script: `CREATE TABLE IF NOT EXISTS tab_categories (
 				id INT AUTO_INCREMENT,
-				uuid VARCHAR(40) NOT NULL,
+				uuid VARCHAR(40) NOT NULL DEFAULT (uuid()),
 				name VARCHAR(300) NOT NULL,
 				description VARCHAR(8000) NULL,
 				active TINYINT(1) NOT NULL DEFAULT 1,
@@ -19,6 +19,7 @@ var (
 				deleted_at TIMESTAMP NULL,
 
 				PRIMARY KEY (id),
+				UNIQUE INDEX UUID_UNIQUE (uuid ASC),
 				UNIQUE INDEX ID_UNIQUE (id ASC)
 			) ENGINE=InnoDB CHARACTER SET=utf8;
 			`,
