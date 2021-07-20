3987d1509cf3b21e266414b0a55809abf9a75aae
Diego Clair <diego93rodrigues@gmail.com>
2021-07-19 22:22:54 -0300

    fix: change structure

---
+++ devops/docker/challenge03-CI-Go/src/soma/go.mod
@@ -0,0 +1,3 @@
+module github.com/diegoclair/microservices-codeeducation/challenge03/soma
+
+go 1.16
--- devops/docker/challenge03-CI-Go/src/soma/main.go
+++ devops/docker/challenge03-CI-Go/src/soma/main.go
@@ -1,9 +1,12 @@
 package main
 
+type somaPackage struct {
+}
+
 func soma(first, second int) int {
 	return first + second
 }
 
 func main() {
-	println(soma(5,5))
+	println(soma(5, 5))
 }
